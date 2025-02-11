d8b9dc045990726ae035f95df8a1ff6a8141b2b7
guojianli <1547702880@qq.com>
2025-02-11 15:54:43 +0800

    feat: init

---
+++ src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+
+@Injectable()
+export class AppService {
+  constructor(private prisma: PrismaService) {}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async getAllUsers() {
+    return this.prisma.user.findMany();
+  }
+}
