e87789a9cf8258a46e15ca08eba323f620aa6893
guojianli <1547702880@qq.com>
2025-02-12 15:37:57 +0800

    chore: update dependencies and remove Prisma configuration

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  constructor(private prisma: PrismaService) {}
-
   getHello(): string {
     return 'Hello World!';
-  }
-
-  async getAllUsers() {
-    return this.prisma.user.findMany();
   }
 }
